e56c51314eee5a36ad5ad6d88e4774029ad0a01c
KJGEOG575 <63277435+KJGEOG575@users.noreply.github.com>
2020-04-18 00:09:24 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-alert("Hello!")
+function initialize(){
+    var $splashCloseButton = $('#splash-close-btn');
+    var $splash = $('#splash');
+    var $headerDropdownMenu = $('#header-dropdown-menu');
+    var $headerDropdownButton = $('#header-button');
+    var $aboutMenuItem = $('#about-menu-item');
+
+    $splashCloseButton.click(function () {
+        $splash.fadeOut('slow');
+    });
+
+    $aboutMenuItem.click(function() {
+        $overlay.fadeIn('slow');
+    });
+
+    $headerDropdownButton.click(function () {
+        $headerDropdownMenu.slideToggle();
+    });
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+}};
+
+$(document).ready(initialize);
