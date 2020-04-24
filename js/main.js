943bff4088e9561f8e1e293ba710fb2c50d6c050
KJGEOG575 <63277435+KJGEOG575@users.noreply.github.com>
2020-04-24 13:27:17 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,25 @@ function initialize(){
     var $headerDropdownMenu = $('#header-dropdown-menu');
     var $headerDropdownButton = $('#header-button');
     var $aboutMenuItem = $('#about-menu-item');
+    var $dataMenuItem = $('#data-menu-item');
+    var $dataSplash = $('.data-page-splash');
+    var $dataCloseButton = $('#data-close-btn');
 
     $splashCloseButton.click(function () {
         $splash.fadeOut('slow');
     });
 
+    $dataCloseButton.click(function () {
+        $dataSplash.fadeOut('slow');
+    });
+
     $aboutMenuItem.click(function() {
-        $overlay.fadeIn('slow');
+        $splash.fadeIn('slow');
+    });
+
+    $dataMenuItem.click(function() {
+        $()
+        $dataSplash.fadeIn('slow');
     });
 
     $headerDropdownButton.click(function () {
